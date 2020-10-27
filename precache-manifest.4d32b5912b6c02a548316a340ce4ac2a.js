3bc59f1bd4955b451e04bd96f8485e4e203326a4
Abhijeet Banerjee <abhijeet.banerjee@hexagonglobal.in>
2020-10-27 10:27:08 +0530

    Updates

---
+++ precache-manifest.4d32b5912b6c02a548316a340ce4ac2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335c47fc9a9bf9f72f4ee6082fb5cc4c",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "b2bb1d3bb8ca853d19ae",
+    "url": "/currency-converter/static/css/main.2e7d052d.chunk.css"
+  },
+  {
+    "revision": "82dcf2b234778fb84a2b",
+    "url": "/currency-converter/static/js/2.04480cee.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/currency-converter/static/js/2.04480cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2bb1d3bb8ca853d19ae",
+    "url": "/currency-converter/static/js/main.1b23c259.chunk.js"
+  },
+  {
+    "revision": "a0730edf8381b13d6dc3",
+    "url": "/currency-converter/static/js/runtime-main.46121f8e.js"
+  }
+]);
